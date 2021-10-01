ffa5368d5ed22cd19ec4476019fe5b097bb68583
FrancescoSica16 <francesco.sica16@gmail.com>
2021-10-01 18:49:23 +0200

    implementazione search bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+   
+    <Searchbar @search="SearchFilm"/>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Searchbar from './components/Searchbar.vue';
+import axios from "axios";
 
 export default {
   name: 'App',
+  data: function (){
+    return {
+      filmList: [],
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Searchbar
+  },
+  computed:{
+
+  },
+  methods:{
+    SearchFilm: function (serchKey) {
+
+      axios.get('https://api.themoviedb.org/3/search/movie/', {
+        params: { 
+          api_key : '3470fd1ace343e7c3044569008e5458d',
+          query: serchKey
+        }})
+      .then( (response) => {
+         this.filmList = response.data.results.slice();
+          console.log(this.filmList);
+    })
+    }
+  },
 }
-</script>
+</script> 
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "style/general.scss";
+@import "style/variabiles.scss";
+
 </style>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <form class="d-flex">
+    <div>
+      <input class="form-control" type="search" placeholder="Cerca il film" id="name" name="name" v-model.trim="needle">
+    </div>
+
+    <div>
+      <button type="submit" class="btn btn-primary" @click.prevent="$emit('search', needle)">Cerca</button>
+    </div>
+   
+  </form>
+</template>
+
+<script>  
+// import axios from "axios"; 
+
+export default {
+  name: 'Searchbar',
+  props: [],
+
+  data(){
+    return {
+      needle:"",
+    }
+  },
+  computed:{
+
+  },
+  mounted(){
+
+  },
+  created(){
+
+  },
+  // methods:{
+  //   getSearch: function () {
+  //     console.log(this.needle)
+  //   }
+  // }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>

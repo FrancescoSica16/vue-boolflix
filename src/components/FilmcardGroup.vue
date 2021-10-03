6e18a0fd71a4cf9c3373bc814177bfcbfbcc2c16
FrancescoSica16 <francesco.sica16@gmail.com>
2021-10-03 22:27:49 +0200

    milestone 2 - flag insert

--- src/components/FilmcardGroup.vue
+++ src/components/FilmcardGroup.vue
@@ -4,7 +4,15 @@
         <div class="col-3 ">
         <h1>{{title_original}}</h1>
         <h3>{{title}}</h3>
-        <p>{{language}}</p>
+        <!-- <p>{{language}}</p> -->
+        <div>
+            <img class="flag" :src="(language === 'it') ? 
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/64px-Flag_of_Italy.svg.png' : 
+            (language === 'en') ?
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/64px-Flag_of_the_United_Kingdom_%283-5%29.svg.png' : 
+            // else
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Flag_of_Union_of_Soviet_Socialist_Republics.png/64px-Flag_of_Union_of_Soviet_Socialist_Republics.png'" alt="">
+        </div>
         <p>{{vote}}</p>         
         </div>
       </div>
@@ -22,5 +30,7 @@ export default {
 </script>
 
 <style>
-
+   img.flag{
+        width: 30px;
+    }
 </style>
